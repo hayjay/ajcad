918facc889fd00270c6295a42de676d0db359b18
Nurudeen Ajayi <ydeveloper15@gmail.com>
2019-12-17 12:32:13 +0100

    New Agent Endpoint

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -6,9 +6,16 @@ module.exports = {
     create (req, res) {
         //first off, create user record for this customer
         return Agent.create({
+            unique_id : (new Date().getTime()).toString(12),
+            pin : req.body.pin,
+            phone_number : req.body.phone_number,
+            // phone_number : req.body.phone_number
         })
-        .then(function(resp){
-
+        .then(function(agent_response){
+            var data = {
+                agent : agent_response,
+            };
+        res.status(400).send(Helper.apiResponseFormat(true, 201, 'Agent Registration successful!', data));
         })
         .catch(function(error) {
             res.status(500).send(error);
--- routes/agent.js
+++ routes/agent.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = function(app){
     let agent = require('../controllers/agentController')
-    app.route('/agent/fund-wallet')
-        .get(agent.fundWallet)
+    app.route('/agent/create')
+        .post(agent.create)
     app.route('/agent/transactions')
         .get(agent.transactions)
     
